3869ee5d895c4c38afec06b35bf6e56c027a504c
Forprix <forprix@mail.ru>
2023-11-05 19:25:54 +0300

    Add new files and directories

---
+++ gfx/glsl/frag/shader1.glsl
@@ -0,0 +1,9 @@
+#version 300 es
+precision highp float;
+#define GLSLIFY 1
+uniform float uTime;
+in vec2 uv;
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(uv.x, uTime * 0.0001, 0.0, 1.0);
+}
